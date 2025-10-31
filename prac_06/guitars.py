072903b1436bfe718f2ed805064842211a3d6618
Jiaxing Tian <jiaxing.tian@my.jcu.edu.au>
2025-10-31 16:08:20 +0800

    Added guitars.py

---
+++ prac_06/guitars.py
@@ -0,0 +1,27 @@
+from prac_06.guitar import Guitar
+
+
+def main():
+    print("My guitars!")
+    name = input("Name: ")
+    guitars = []
+    while name!= "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        add_guitar = Guitar(name, year, cost)
+        guitars.append(add_guitar)
+        print(f"{name} ({year}) : {cost} added.")
+        name = input("Name: ")
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        if guitar.is_vintage():
+            vintage_string = " (vintage)"
+        else:
+            vintage_string = ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
+main()
