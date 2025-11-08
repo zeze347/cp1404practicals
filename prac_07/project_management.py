3f6ce624878e2d4e80b74dd6e91e3cbbae3aead2
Jiaxing Tian <jiaxing.tian@my.jcu.edu.au>
2025-11-08 14:27:25 +0800

    Add docstrings and improve the program according to the warnings from pylint.

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -3,13 +3,14 @@ Program: project_management.py
 Estimate: 5 hours
 Actual:  7 hours
 """
-from project import Project
 import datetime
+from project import Project
 
 FILENAME = 'projects.txt'
 
 
 def main():
+    """Print the welcome message and the menu. Users can then choose which function to execute."""
     print("Welcome to Pythonic Project Management")
     projects = load_projects(FILENAME)
     print(f"Loaded {len(projects)} projects from {FILENAME}")
@@ -43,8 +44,9 @@ def main():
 
 
 def load_projects(filename):
+    """Load the project data file and store it in the memory.  """
     projects = []
-    with open(filename, 'r') as infile:
+    with open(filename, 'r', encoding='utf-8') as infile:
         infile.readline()
         for line in infile:
             line = line.strip()
@@ -54,11 +56,20 @@ def load_projects(filename):
             priority = int(parts[2])
             cost_estimate = float(parts[3])
             completion_percentage = int(float(parts[4]))
-            projects.append(Project(name, start_date, priority, cost_estimate, completion_percentage))
+            projects.append(
+                Project(
+                    name,
+                    start_date,
+                    priority,
+                    cost_estimate,
+                    completion_percentage
+                )
+            )
         return projects
 
 
 def display_menu():
+    """Display the program menu"""
     menu = """- (L)oad projects
 - (S)ave projects
 - (D)isplay projects
@@ -70,6 +81,7 @@ def display_menu():
 
 
 def display_projects(projects):
+    """Display completed and incomplete projects"""
     incomplete = [project for project in projects if not project.is_complete()]
     complete = [project for project in projects if project.is_complete()]
 
@@ -85,6 +97,7 @@ def display_projects(projects):
 
 
 def add_new_project(projects):
+    """Add a new project to the list of projects."""
     print("Let's add a new project")
     name = input("Name: ")
     start_date = input("Start date (dd/mm/yy): ")
@@ -95,6 +108,7 @@ def add_new_project(projects):
 
 
 def update_project(projects):
+    """Update the percentage and priority of the project"""
     for i, project in enumerate(projects):
         print(i, project.format_for_display())
 
@@ -113,28 +127,31 @@ def update_project(projects):
 
 
 def filter_projects_by_date(projects):
+    """Filter the projects by date"""
     try:
-        user_date = input("Show projects that start after date (dd/mm/yy): ")
-        user_date = parse_date(user_date)
-        projects_after_user_date = []
+        date = input("Show projects that start after date (dd/mm/yy): ")
+        date = parse_date(date)
+        projects_after_date = []
         for project in projects:
             project_start_date = parse_date(project.start_date)
-            if project_start_date >= user_date:
-                projects_after_user_date.append(project)
-        projects_after_user_date.sort(key=lambda single_project: parse_date(single_project.start_date))
-        for project in projects_after_user_date:
+            if project_start_date >= date:
+                projects_after_date.append(project)
+        projects_after_date.sort(key=lambda single_project: parse_date(single_project.start_date))
+        for project in projects_after_date:
             print(project.format_for_display())
     except ValueError:
         print("Dates must be in dd/mm/yy format")
 
 
 def parse_date(date_string):
+    """Parse a date string into a datetime object."""
     parsed_date = datetime.datetime.strptime(date_string.strip(), "%d/%m/%Y").date()
     return parsed_date
 
 
 def save_projects(filename, projects):
-    with open(filename, 'w') as outfile:
+    """Save the projects data to file"""
+    with open(filename, 'w', encoding='utf-8') as outfile:
         outfile.write("Name\tStart Date\tPriority\tCost Estimate\tCompletion Percentage\n")
         for project in projects:
             outfile.write(
